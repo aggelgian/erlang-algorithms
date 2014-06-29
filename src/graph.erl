22dab2b6d504ecf123eda891dd8c045793e076fc
Aggelos Giantsios <aggelgian@gmail.com>
2014-06-29 14:20:05 +0300

    Update copyright years

--- src/graph.erl
+++ src/graph.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright © 2013 Aggelos Giantsios
+%% Copyright © 2013-2014 Aggelos Giantsios
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
 %% and associated documentation files (the “Software”), to deal in the Software without restriction, 
@@ -21,7 +21,7 @@
 %% %CopyrightEnd%
 %%
 
-%% @copyright 2013 Aggelos Giantsios
+%% @copyright 2013-2014 Aggelos Giantsios
 %% @author Aggelos Giantsios
 
 %% ============================================================================
